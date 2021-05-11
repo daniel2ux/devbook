516c5d57ee1a4b823976b0577bc3494025bfd668
Daniel Paulino <daniel2ux@gmail.com>
2021-05-11 18:08:53 -0300

    Add SQL dados

---
+++ api/src/sql/dados.sql
@@ -0,0 +1,41 @@
+INSERT INTO
+    users (name, nick, email, password)
+VALUES
+    (
+        "User 1",
+        "userone",
+        "user1@email.com",
+        "$2a$10$4VvzTO/hrykwVFIzZKq5guBGZGde/9V.SOlo4e1SyUD3lKtXjYRmu"
+    ),
+    (
+        "User 2",
+        "usertwo",
+        "user2@email.com",
+        "$2a$10$4VvzTO/hrykwVFIzZKq5guBGZGde/9V.SOlo4e1SyUD3lKtXjYRmu"
+    ),
+    (
+        "User 3",
+        "userthree",
+        "user3@email.com",
+        "$2a$10$4VvzTO/hrykwVFIzZKq5guBGZGde/9V.SOlo4e1SyUD3lKtXjYRmu"
+    ),
+    (
+        "User 4",
+        "userfour",
+        "user4@email.com",
+        "$2a$10$4VvzTO/hrykwVFIzZKq5guBGZGde/9V.SOlo4e1SyUD3lKtXjYRmu"
+    ),
+    (
+        "User 5",
+        "userfive",
+        "user5@email.com",
+        "$2a$10$4VvzTO/hrykwVFIzZKq5guBGZGde/9V.SOlo4e1SyUD3lKtXjYRmu"
+    );
+
+-- INSERT INTO
+--     followers (user_id, follower_id)
+-- VALUES
+--     (1, 2),
+--     (1, 4),
+--     (2, 3),
+--     (3, 2);
