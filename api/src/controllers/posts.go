a9fb8cf762e6f215cef864e7b0bd282b040d0dc4
Daniel <daniel2ux@gmail.com>
2021-05-17 23:43:17 -0300

    Create Post

--- api/src/controllers/posts.go
+++ api/src/controllers/posts.go
@@ -1,9 +1,53 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/answers"
+	"api/src/auth"
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 func NewPost(w http.ResponseWriter, r *http.Request) {
+	userID, err := auth.GetUserID(r)
+	if err != nil {
+		answers.Error(w, http.StatusUnauthorized, err)
+		return
+	}
 
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		answers.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var post models.Post
+	if err := json.Unmarshal(body, &post); err != nil {
+		answers.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	post.AuthorID = userID
+
+	db, err := database.Connect()
+	if err != nil {
+		answers.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repo := repositories.PostRepository(db)
+	postID, err := repo.Create(post)
+	if err != nil {
+		answers.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	post.ID = postID
+	answers.JSON(w, http.StatusCreated, post)
 }
 
 func GetPosts(w http.ResponseWriter, r *http.Request) {
