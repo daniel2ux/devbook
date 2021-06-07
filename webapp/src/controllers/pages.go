c979d45e0f684fd667f1d086db20ca9b76ea1435
Daniel Paulino <daniel.r.paulino@outlook.com>
2021-06-06 23:43:16 -0300

    Get posts

--- webapp/src/controllers/pages.go
+++ webapp/src/controllers/pages.go
@@ -1,7 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"webapp/src/answers"
+	"webapp/src/config"
+	"webapp/src/models"
+	"webapp/src/requests"
 	"webapp/src/utils"
 )
 
@@ -14,5 +20,25 @@ func LoadUserEnrollPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func LoadMainPage(w http.ResponseWriter, r *http.Request) {
-	utils.LoadTemplate(w, "home.html", nil)
+	url := fmt.Sprintf("%s/posts", config.APIURL)
+	response, err := requests.RequestWithAuth(r, http.MethodGet, url, nil)
+	if err != nil {
+		answers.JSON(w, http.StatusInternalServerError, answers.APIError{Error: err.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		answers.CheckStatusCodeError(w, response)
+		return
+	}
+
+	var posts []models.Post
+
+	if err := json.NewDecoder(response.Body).Decode(&posts); err != nil {
+		answers.JSON(w, http.StatusBadRequest, err)
+		return
+	}
+
+	utils.LoadTemplate(w, "home.html", posts)
 }
