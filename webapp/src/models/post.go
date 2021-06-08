52d7876bcfe6152a62d8af236dc2820d4e5bb565
Daniel Paulino <daniel.r.paulino@outlook.com>
2021-06-07 23:50:42 -0300

    load posts on main page

--- webapp/src/models/post.go
+++ webapp/src/models/post.go
@@ -6,8 +6,8 @@ type Post struct {
 	ID         int64     `json:"id,omitempty"`
 	Title      string    `json:"title,omitempty"`
 	Content    string    `json:"content,omitempty"`
-	AuthorID   uint64    `json:"author_id,omitempty"`
-	AuthorNick string    `json:"author_nick,omitempty"`
-	Likes      uint64    `json:"likes,omitempty"`
-	CreateAt   time.Time `json:"create_at,omitempty"`
+	AuthorID   uint64    `json:"authorId,omitempty"`
+	AuthorNick string    `json:"authorNick,omitempty"`
+	Likes      uint64    `json:"likes"`
+	CreateAt   time.Time `json:"createAt,omitempty"`
 }
